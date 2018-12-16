ad3bbe1029496e7c00ab833540e3f2e0f0da8403
Zachary Boyd <zachary@zacharyboyd.nyc>
2018-12-16 14:47:05 -0500

    working screen lock

--- src/CommandTypes/LockScreen.ts
+++ src/CommandTypes/LockScreen.ts
@@ -1,8 +1,8 @@
 import { CommandTypeBase, CommandOptions } from "face-command-common";
-import { LockScreen as DoLock } from "screen-lock";
+import { lockScreen } from "lock-screen";
 
 export default class LockScreen extends CommandTypeBase {
     public async Run(commandOptions: CommandOptions): Promise<any> {
-        DoLock();
+        lockScreen();
     }
 }
