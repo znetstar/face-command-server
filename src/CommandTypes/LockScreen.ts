d938050241c8fab6fd88370ad671a93b178d29ff
Zachary Boyd <zachary@zacharyboyd.nyc>
2018-12-16 01:26:18 -0500

    command execution complete

--- src/CommandTypes/LockScreen.ts
+++ src/CommandTypes/LockScreen.ts
@@ -1,8 +1,8 @@
-import { IAppResources, CommandTypeBase, CommandOptions } from "face-command-common";
-import AppResources from "../AppResources";
+import { CommandTypeBase, CommandOptions } from "face-command-common";
+import { LockScreen as DoLock } from "screen-lock";
 
 export default class LockScreen extends CommandTypeBase {
     public async Run(commandOptions: CommandOptions): Promise<any> {
-        
+        DoLock();
     }
 }
