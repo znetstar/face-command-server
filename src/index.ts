503c4e975262c53cad37863b8e09a2efd42e9e1b
Zachary Boyd <zachary@zacharyboyd.nyc>
2019-01-03 01:23:44 -0500

    Adds launch script and npm start. Adds comments to DefaultConfiguratiton.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { default as DetectionService } from "./DetectionService";
 export { default as FaceManagementService } from "./FaceManagementService";
 export { default as CommandService } from "./CommandService";
 export { default as ConfigService } from "./ConfigService";
+export { default as LogsService } from "./LogsService";
 export { CommandExecutionError } from "./Errors";
 export  { default as FaceCapture, NoFacesDetectedError, TooManyFacesError } from "./FaceCapture";
 export { Main as Main } from "./launch";
